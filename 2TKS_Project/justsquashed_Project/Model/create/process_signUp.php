69af8f88ad0f51e991890594d950c357cbcf2742
Sipiwe <scmarako@gmail.com>
2019-10-12 03:01:06 +0200

    SignUp modified

--- 2TKS_Project/justsquashed_Project/Model/create/process_signUp.php
+++ 2TKS_Project/justsquashed_Project/Model/create/process_signUp.php
@@ -10,14 +10,27 @@
    $member = "Non Member";
    
     include '../connection.php';
+    if($custPassWord != $confirm_password){
+        echo "<script type='text/javascript'>alert('Password does not match!');</script>";
+        ?>
+    <script type="text/javascript">location.href = '/2TKS_Project/justsquashed_Project/View/signUp.php';</script>
+
+    }
     $result = "INSERT INTO customer
     VALUES('$custUserName','$firstname','$lastname','$email','$phone','$age ','$custPassWord','$member')";
     $que =mysqli_query($conn,$result);
     if($que== true) 
     {
-        header('Location: /2TKS_Project/justsquashed_Project/View/login.php');
+        echo "<script type='text/javascript'>alert('Sign up successful. You may login using your Username and Password.');</script>";
+        ?>
+    <script type="text/javascript">location.href = '/2TKS_Project/justsquashed_Project/View/login.php';</script>
+    <?php
+        
     }else
     {
-        header('Location: /2TKS_Project/justsquashed_Project/View/signUp.php');
+        echo "<script type='text/javascript'>alert('Sign up Unsuccessful.');</script>";
+        ?>
+    <script type="text/javascript">location.href = '/2TKS_Project/justsquashed_Project/View/signUp.php';</script>
+    <?php
     }
 ?>
